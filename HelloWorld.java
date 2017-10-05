64b6b8c54ee9b920b2aeb173bfc2561edd032ee8
nehadalal1787 <neha.dalal1787@gmail.com>
2017-10-05 12:05:21 +0530

    executed class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,8 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-	For(i=1;i<=10;i++)
-	{
-		
+
       		System.out.println("Hello, World");
-	}
+
    }
 }
