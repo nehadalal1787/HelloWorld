fe7d8157768d4d0f10e724e561476fced9f991c3
nehadalal1787 <neha.dalal1787@gmail.com>
2017-10-05 17:50:35 +0530

    Modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,6 @@ public class HelloWorld {
 public int i;
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-		for(i=1;i<=10;i++)
-{
       		System.out.println("Hello, World", +i);
-}
    }
 }
